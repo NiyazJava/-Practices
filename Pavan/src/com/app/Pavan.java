691099aeddc80f015d6a07c9c8373b71c056b959
win10 <win10@DESKTOP-RN2M29F>
2018-02-27 02:53:24 +0530

    commit by niyaz

---
+++ Pavan/src/com/app/Pavan.java
@@ -0,0 +1,14 @@
+package com.app;
+
+public class Pavan {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c;
+		c=a+b;
+		System.out.println("the sum of two number is::"+c);
+
+	}
+
+}
